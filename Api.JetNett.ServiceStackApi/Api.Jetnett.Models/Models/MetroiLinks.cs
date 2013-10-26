14daa24c0185aa76430bbe1ae4221b4427727ce9
Kyle Gobel <kgobel@gmail.com>
2013-10-25 22:34:03 -0400

    added metroilinks to models

--- Api.JetNett.ServiceStackApi/Api.Jetnett.Models/Models/MetroiLinks.cs
+++ Api.JetNett.ServiceStackApi/Api.Jetnett.Models/Models/MetroiLinks.cs
@@ -1,34 +1,36 @@
 using System;
-using System.Collections.Generic;
+using ServiceStack.DataAnnotations;
 
 namespace Api.Jetnett.Models.Models
 {
-    public partial class Metro_iLinks
+    [Alias("Metro_iLinks")]
+    public class MetroiLinks
     {
-        public int ID { get; set; }
-        public int Client_ID { get; set; }
-        public string PageBGColor { get; set; }
+        public int Id { get; set; }
+        [Alias("Client_ID")]
+        public int ClientId { get; set; }
+
+        public string PageBgColor { get; set; }
         public string PageTextColor { get; set; }
         public string PageLinkColor { get; set; }
         public string HomeSearchText { get; set; }
-        public string HomeSearchURL { get; set; }
+        public string HomeSearchUrl { get; set; }
         public string OriginationPage { get; set; }
         public string BackLinkTitle { get; set; }
-        public string BackLinkURL { get; set; }
+        public string BackLinkUrl { get; set; }
         public string BackLinkTarget { get; set; }
-        public string DropDownPageIDs { get; set; }
+        public string DropDownPageIds { get; set; }
         public string ClientLogoGraphicLocation { get; set; }
-        public string ClientLogoLinkURL { get; set; }
+        public string ClientLogoLinkUrl { get; set; }
         public string ClientLogoTargetWindow { get; set; }
         public string ClientLogoAltText { get; set; }
         public string ProductLogoGraphicLocation { get; set; }
-        public string ProductLogoLinkURL { get; set; }
+        public string ProductLogoLinkUrl { get; set; }
         public string ProductLogoTargetWindow { get; set; }
         public string ProductLogoAltText { get; set; }
         public string OriginationPageTarget { get; set; }
-        public string SEOMetaKeys { get; set; }
-        public string SEOMetaDesc { get; set; }
+        public string SeoMetaKeys { get; set; }
+        public string SeoMetaDesc { get; set; }
         public Nullable<int> FontSizePx { get; set; }
-        public virtual Client Client { get; set; }
     }
 }
