7c00d94c66792281806777dfd55115a722ba7721
Kyle Gobel <kgobel@gmail.com>
2013-10-27 03:24:02 -0400

    cleaned up whitespace

--- Api.JetNett.ServiceStackApi/Api.JetNett.ServiceStackApi/ClientService.cs
+++ Api.JetNett.ServiceStackApi/Api.JetNett.ServiceStackApi/ClientService.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using Api.JetNett.Models.Operations;
+﻿using Api.JetNett.Models.Operations;
 using Api.JetNett.Models.Types;
 using Api.JetNett.ServiceStackApi.Operations;
-using ServiceStack.Common.Web;
 
 namespace Api.JetNett.ServiceStackApi
 {
     public class ClientService : JetNettService<ClientRequestDTO,ClientResponseDTO,Client>
-    {
-        
-    }
+    { }
 }
