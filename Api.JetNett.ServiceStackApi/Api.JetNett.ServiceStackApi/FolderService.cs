e69c66a11f0f2d6ca043c46aad66fe83544592be
Kyle Gobel <kgobel@gmail.com>
2013-11-23 16:32:27 -0500

    added folder service

---
+++ Api.JetNett.ServiceStackApi/Api.JetNett.ServiceStackApi/FolderService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Api.JetNett.Models.Operations;
+using Api.JetNett.Models.Types;
+using Api.JetNett.ServiceStackApi.Operations;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceHost;
+
+namespace Api.JetNett.ServiceStackApi
+{
+    public class FolderService : JetNettService<FolderRequestDTO, FolderResponseDTO, Folder>
+    {
+        public FolderService(IDbConnectionFactory dbConnectionFactory) : base(dbConnectionFactory)
+        { }
+
+        public override FolderResponseDTO Get(FolderRequestDTO request)
+        {
+            
+            if (default(int) != request.ParentId)
+            {
+                return new FolderResponseDTO
+                {
+                    Entities = Db.Where<Folder>(f => f.ParentFolderId == request.ParentId)
+                };
+            }
+            return base.Get(request);
+        }
+    }
+}
---
+++ Api.JetNett.ServiceStackApi/Api.Jetnett.Models/Operations/Folder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api.JetNett.Models.Contracts;
+using Api.JetNett.Models.Types;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace Api.JetNett.Models.Operations
+{
+    [Api("GET all Clients or GET or DELETE a single Client by Id. Use POST to create a new Client and PUT to update it.")]
+    [Route("/folder/{Id}", "GET")]
+    [Route("/folder", "GET, POST, PUT, PATCH, DELETE")]
+    [Route("/folder/children/{ParentId}", "GET")]
+    public class FolderRequestDTO : IRequestDTO<Folder>, IReturn<Folder>
+    {
+        public FolderRequestDTO()
+        {
+            Entity = new Folder();
+        }
+        public int Id { get; set; }
+
+        public int ParentId { get; set; }
+        public Folder Entity { get; set; }
+    }
+
+    public class FolderResponseDTO : IResponseDTO<Folder>, IHasResponseStatus
+    {
+        public FolderResponseDTO()
+        {
+            this.ResponseStatus = new ResponseStatus();
+            this.Entity = new Folder();
+            this.Entities = new List<Folder>();
+        }
+
+        public ResponseStatus ResponseStatus { get; set; }
+
+        public Folder Entity { get; set; }
+        public List<Folder> Entities { get; set; }
+    }
+}
