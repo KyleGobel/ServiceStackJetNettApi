450e1ac94f04d026657710a48bf10b934b7ca56d
Kyle Gobel <kgobel@gmail.com>
2013-11-24 22:39:35 -0500

    added page/path route and page/folder routes

--- Api.JetNett.ServiceStackApi/Api.JetNett.ServiceStackApi/PagesService.cs
+++ Api.JetNett.ServiceStackApi/Api.JetNett.ServiceStackApi/PagesService.cs
@@ -1,4 +1,5 @@
-﻿using Api.JetNett.Models.Operations;
+﻿using System.Linq;
+using Api.JetNett.Models.Operations;
 using Api.JetNett.Models.Types;
 using Api.JetNett.ServiceStackApi.Operations;
 using ServiceStack.OrmLite;
@@ -10,6 +11,43 @@ namespace Api.JetNett.ServiceStackApi
     { 
         public PagesService(IDbConnectionFactory dbConnectionFactory) : base(dbConnectionFactory)
         { }
-        
+
+        public override PagesResponseDTO Get(PagesRequestDTO request)
+        {
+            if (request.FolderId != default(int))
+            {
+                return new PagesResponseDTO
+                {
+                    Entities = Repository.Where(x => x.FolderId == request.FolderId)
+                };
+            }
+            if (request.PathPageId != default(int))
+            {
+                var page = Repository.GetById(request.PathPageId);
+
+                var folderPath = GetFolderPath(page.FolderId);
+
+                page.Title = folderPath + page.Title;
+                return new PagesResponseDTO
+                {
+                    Entity = page
+                };
+            }
+            return base.Get(request);
+        }
+
+        private string GetFolderPath(int? folderId, string path = "")
+        {
+            if (folderId == null || folderId == 0)
+                return path;
+
+            var folder = Db.Where<Folder>(x => x.Id == folderId).SingleOrDefault();
+            if (folder != null)
+            {
+                path = path.Insert(0, folder.Name + ",");
+                return GetFolderPath(folder.ParentFolderId, path);
+            }
+            return "";
+        }
     }
 }
--- Api.JetNett.ServiceStackApi/Api.Jetnett.Models/Operations/Pages.cs
+++ Api.JetNett.ServiceStackApi/Api.Jetnett.Models/Operations/Pages.cs
@@ -10,6 +10,8 @@ namespace Api.JetNett.Models.Operations
         [Api("GET all Pages or GET or DELETE a single Page by Id. Use POST to create a new Page and PUT to update it.")]
         [Route("/page/{Id}", "GET")]
         [Route("/page", "GET, POST, PUT, PATCH, DELETE")]
+        [Route("/page/folder/{folderId}", "GET")]
+        [Route("/page/path/{pathPageId}", "GET")]
         public class PagesRequestDTO : IRequestDTO<Page>, IReturn<PagesResponseDTO>
         {
             public PagesRequestDTO()
@@ -17,6 +19,8 @@ namespace Api.JetNett.Models.Operations
                 Entity = new Page();
             }
             public int Id { get; set; }
+            public int PathPageId { get; set; }
+            public int FolderId { get; set; }
             public string Username { get; set; }
             public string Password { get; set; }
             public Page Entity { get; set; }
